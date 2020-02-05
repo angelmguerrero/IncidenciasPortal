553aa67db8903fe6468b72039c9c1d79eca67e0b
Angel <angelmguerrero@hotmail.com>
2020-02-05 08:41:34 -0600

    Incidencias class

---
+++ Coqueta.Portal.Web.Entidades/Dominio/Incidencias/Incidencias.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coqueta.Incidencias.Web.Entidades.Dominio.Incidencias
+{
+    public class Incidencias
+    {
+        public string Id { get; set; }
+        public string Lote { get; set; }
+        public Nullable<short> Riel { get; set; }
+        public string Semana { get; set; }
+        public Nullable<int> TipoIncidencia { get; set; }
+        public Nullable<int> TotalIncidencias { get; set; }
+        public string Descripcion { get; set; }
+        public Nullable<int> UsuarioId { get; set; }
+        
+    }
+}
